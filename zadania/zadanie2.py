9b4e5e0011b78254f2dad73594e5ca3f416a9150
Krystian <Krystiano8686@gmail.com>
2020-03-26 16:16:27 +0100

    Zadanie2

---
+++ zadania/zadanie2.py
@@ -0,0 +1,21 @@
+a = 3
+b = 4
+c = 5
+
+dodawanie = (a+b)
+print(dodawanie)
+
+dzielenie = (c/b)
+print(dzielenie)
+
+mnozenie = (a*b*c)
+print(mnozenie)
+
+odejmowanie = (c-a)
+print(odejmowanie)
+
+potega = pow(a, b)
+print(potega)
+
+dzielenie_calkowite = c // a
+print(dzielenie_calkowite)
