d0b9e41c6bdf953b605c41f5111854a1f3a54eac
Krystian <Krystiano8686@gmail.com>
2020-03-31 14:18:08 +0200

    Zadanie 6

--- zadania/zadanie6.py
+++ zadania/zadanie6.py
@@ -1,2 +1,2 @@
-tekst = ("la la la la la")
+tekst = ("La la, la la la la la na na na na na   La la na na la la la la la na na na na na I find a way to block it I go La la na na la la la la la na na na na na La la na…")
 print(str.count(tekst, "la"))
