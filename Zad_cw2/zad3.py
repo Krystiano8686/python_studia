7508abe841d1ab82668ceee29a70a7c1bc61199d
Krystian <Krystiano8686@gmail.com>
2020-05-14 14:15:51 +0200

    Praca domowa

---
+++ Zad_cw2/zad3.py
@@ -0,0 +1,8 @@
+# ZAD3
+
+print(" a == b \ równe sobie")
+print(" a != b \ różne od siebie")
+print(" a < b \ a mniejsze od b")
+print(" a > b \ a większe od b")
+print(" a <= b \ a mniejsze bądź równe b")
+print(" a >= b \ a większe bądź równe b")
