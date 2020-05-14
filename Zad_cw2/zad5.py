280aaca36585117bcdd0aa851d73616a7f93c4d9
Krystian <Krystiano8686@gmail.com>
2020-05-14 14:54:52 +0200

    Praca domowa

---
+++ Zad_cw2/zad5.py
@@ -0,0 +1,11 @@
+# ZAD5
+
+a, b, c = input('Podaj 3 liczby: '), input(), input()
+a = float(a)
+b = float(b)
+c = float(c)
+
+if a <= 10 and a >= 0 and a > b and b > c:
+    print("Wszystkie warunki zostały spełnione")
+else:
+    print("Warunki nie zostały spełnione")
