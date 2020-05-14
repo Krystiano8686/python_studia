2dfe36c2dd5f45fe6079af8136e5ec63532a3f16
Krystian <Krystiano8686@gmail.com>
2020-05-14 15:38:31 +0200

    Praca domowa

---
+++ Zad_cw2/zad8.py
@@ -0,0 +1,11 @@
+# ZAD8
+
+liczby = int(input('Podaj ilosc liczb wprowadzonych do listy: '))
+lista = []
+
+i = 0
+while i < liczby:
+    i += 1
+    x = int(input("Podaj liczebe: "))
+    lista.append(x)
+print(lista)
