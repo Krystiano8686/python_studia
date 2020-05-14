26808d618fa5b7663eca4a051ae433f50e7df795
Krystian <Krystiano8686@gmail.com>
2020-05-14 14:07:06 +0200

    Praca domowa

--- Zad_cw2/zad2.py
+++ Zad_cw2/zad2.py
@@ -1,4 +1,10 @@
 # ZAD2
 
 import sys
-print("Podaj dwie liczby")
+print("Podaj dwie liczby: ")
+a = sys.stdin.readline()
+b = sys.stdin.readline()
+a = float(a)
+b = float(b)
+c = a * b
+sys.stdout.write(f"Wynik Twojego mnozenia wynosi: {c}")
